a63a6bf7d45ac3d0a06d1fd718a29cdd8af60f87
Jefferson Uchôa Ponte <jefponte@gmail.com>
2020-11-09 10:19:16 -0300

    Finalização.

--- src/index.php
+++ src/index.php
@@ -37,14 +37,15 @@ echo '
 if(isset($_SESSION['ATIVO'])){
     echo '
 <header>
- <!-- Fixed navbar -->
+ 
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Fixed navbar</a>
+        <a class="navbar-brand" href="#">SimplesAdminPG</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav mr-auto">
+            <!--
             <li class="nav-item active">
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
@@ -54,6 +55,7 @@ if(isset($_SESSION['ATIVO'])){
             <li class="nav-item">
               <a class="nav-link disabled" href="#">Disabled</a>
             </li>
+            -->
           </ul>
         <form id="form-dbname" class="form-inline mt-2 mt-md-0">
             <select class="form-control mr-sm-2" type="text" aria-label="Search" name="dbname">
